d0298ad529446a03f830147531d2304d925140b1
MiquelTactile <miquel@tactile.dk>
2023-01-08 12:05:42 +0100

    Add first assert for element displayed

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import time
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -9,13 +7,20 @@ __doc__ = """
     
 """
 
-# Go to saucedemo.com
-driver = webdriver.Chrome(executable_path="C:\repos\SeleniumWithPython\chromedriver.exe")
-driver.get("https://www.saucedemo.com/")
 
-# Login with standard_user and secret_sauce credentials
-driver.find_element(By.XPATH, "//input[@placeholder='Username']").send_keys("standard_user")
-driver.find_element(By.XPATH, "//input[@placeholder='Password']").send_keys("secret_sauce")
-driver.find_element(By.XPATH, "//input[@data-test='login-button']").click()
-time.sleep(5)
-driver.close()
+def run_test(driver):
+    # Go to saucedemo.com
+    driver.get("https://www.saucedemo.com/")
+
+    # Login with standard_user and secret_sauce credentials
+    driver.find_element(By.XPATH, "//input[@placeholder='Username']").send_keys("standard_user")
+    driver.find_element(By.XPATH, "//input[@placeholder='Password']").send_keys("secret_sauce")
+    driver.find_element(By.XPATH, "//input[@data-test='login-button']").click()
+    assert driver.find_element(By.XPATH, "//span[@class='title']").is_displayed(), True
+
+
+try:
+    driver = webdriver.Chrome(executable_path="C:\repos\SeleniumWithPython\chromedriver.exe")
+    run_test(driver)
+except Exception as e:
+    print(f"Error occurred with exception: {e}")
